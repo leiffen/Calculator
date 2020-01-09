ef8283838370c910f9b824215db95d4a231caa28
Nathan Chau <nathankcchau@gmail.com>
2020-01-08 23:46:48 -0500

    Added new button, few error checking, and clears formula after one is calculated

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String formula = '';
   String result = '';
+  List<String> prevResult = [];
+  bool calculated = false;
 
   RegExp ops = new RegExp('[+|\\-|x|\u{00F7}]');
   RegExp opsNoSub = new RegExp('[+|x|\u{00F7}]');
+  RegExp nums = new RegExp('[1-9]');
 
   String calculate() {
     double num1;
@@ -38,15 +41,30 @@ class _MyHomePageState extends State<MyHomePage> {
     int nextOpI;
     String f = formula;
 
-    RegExp re = RegExp('[x|\u{00F7}]');
+    RegExp mulDiv = new RegExp('[x|\u{00F7}]');
+    RegExp plusMin = new RegExp('[+|\\-]');
+
+    RegExp re = mulDiv;
 
     int operator = f.indexOf(re);
-    while (f.contains(ops)) {
+
+    while (operator > 0 ||
+        (operator == 0 && f.indexOf(re, 1) != -1) ||
+        (operator == -1 && re == mulDiv)) {
       if (operator == -1) {
-        re = RegExp('[+|\\-]');
+        re = plusMin;
       } else {
+        if (operator == 0) {
+          operator = f.indexOf(re, 1);
+        }
         // Find previous operator index
         preOpI = f.substring(0, operator).lastIndexOf(ops);
+
+        if (preOpI == 0 ||
+            (preOpI > -1 && f[preOpI] == '-' && f[preOpI - 1].contains(ops))) {
+          preOpI -= 1;
+        }
+
         // Get first number
         num1 = double.parse(f.substring(preOpI + 1, operator));
 
@@ -63,14 +81,14 @@ class _MyHomePageState extends State<MyHomePage> {
         num2 = double.parse(f.substring(operator + 1, nextOpI));
 
         double r = 0;
-        if (f[operator] == '+') {
-          r = num1 + num2;
-        } else if (f[operator] == '-') {
-          r = num1 - num2;
-        } else if (f[operator] == 'x') {
+        if (f[operator] == 'x') {
           r = num1 * num2;
         } else if (f[operator] == '\u{00F7}') {
           r = num1 / num2;
+        } else if (f[operator] == '+') {
+          r = num1 + num2;
+        } else if (f[operator] == '-') {
+          r = num1 - num2;
         }
 
         f = f.replaceRange(preOpI + 1, nextOpI, r.toString());
@@ -87,27 +105,49 @@ class _MyHomePageState extends State<MyHomePage> {
     return f;
   }
 
-  buttonPressed(String buttonText) {
-    // CLEAR button
+  specialButtonPressed(String buttonText) {
     if (buttonText == 'CLR') {
       setState(() {
         formula = '';
       });
-
-      // DELETE button
     } else if (buttonText == 'DEL') {
       if (formula.isNotEmpty) {
         setState(() {
           formula = formula.substring(0, formula.length - 1);
         });
       }
+    } else if (formula.isNotEmpty) {
+      if (buttonText == '90%') {
+        setState(() {
+          formula += 'x.9';
+        });
+      } else if (buttonText == 'x1.23') {
+        setState(() {
+          formula += 'x1.23';
+        });
+      }
+    }
+    try {
+      calculatePreview(buttonText);
+    } catch (e) {
+      print('error for preview');
+    }
+  }
 
-      // EQUALS button
-    } else if (buttonText == '=') {
-      setState(() {
-        formula = calculate();
-        result = '';
-      });
+  buttonPressed(String buttonText) {
+    if (buttonText == '=') {
+      try {
+        if (formula.length > 0) {
+          setState(() {
+            formula = calculate();
+            result = '';
+            prevResult.clear();
+            calculated = true;
+          });
+        }
+      } catch (e) {
+        print('error on equals');
+      }
     } else {
       if (buttonText.contains(opsNoSub) && formula.length == 0) {
         // Do nothing
@@ -116,9 +156,10 @@ class _MyHomePageState extends State<MyHomePage> {
           formula.length == 1) {
         // Do nothing
       } else if (buttonText == '-' &&
-          formula.length > 2 &&
-          formula[formula.length - 1] == '-' &&
-          formula[formula.length - 2].contains(ops)) {
+          ((formula.length > 2 &&
+              formula[formula.length - 1] == '-' &&
+              formula[formula.length - 2].contains(ops)) ||
+              (formula.length == 1 && formula.endsWith('-')))) {
         // Do nothing
       } else if (buttonText.contains(opsNoSub) &&
           formula.length > 2 &&
@@ -138,10 +179,10 @@ class _MyHomePageState extends State<MyHomePage> {
       } else if (buttonText.contains('0') &&
           formula.endsWith('0') &&
           ((formula.lastIndexOf('.') == -1 &&
-                  !formula.contains(RegExp('[1-9]'))) ||
+              !formula.contains(nums)) ||
               (formula.lastIndexOf('.') < formula.lastIndexOf(ops) &&
                   !formula.contains(
-                      RegExp('[1-9]'), formula.lastIndexOf(ops))))) {
+                      nums, formula.lastIndexOf(ops))))) {
         // Do nothing
       } else {
         int i = formula.lastIndexOf(ops);
@@ -150,6 +191,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 (i == -1 && formula.contains('.')))) {
           // Do nothing
         } else {
+          if (calculated && buttonText.contains(RegExp('.|[1-9]'))) {
+            setState(() {
+              formula = '';
+              calculated = false;
+            });
+          }
           setState(() {
             formula += buttonText;
           });
@@ -157,67 +204,96 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
 
+    try {
+      calculatePreview(buttonText);
+    } catch (e) {
+      print('error for preview');
+    }
+  }
+
+  calculatePreview(String buttonText) {
     // Calculate the preview result of the formula
     if (formula.isNotEmpty) {
       if (buttonText != '=' &&
           !formula[formula.length - 1]
               .contains(new RegExp('[+|\\-|x|\u{00F7}|.]'))) {
-        setState(() {
-//          result = calculate();
-        });
+        if (buttonText != 'DEL') {
+          setState(() {
+            result = calculate();
+            prevResult.add(result);
+          });
+        }
+      } else {
+        if (buttonText == 'DEL') {
+          setState(() {
+            prevResult.removeLast();
+            result = prevResult.last;
+          });
+        }
       }
       // Clear result when formula is empty
     } else {
       setState(() {
         result = '';
+        prevResult = [];
       });
     }
-
-    print(formula);
+//    print('result ' + result);
+//    print('prevResult ' + prevResult.toString());
+//    print('');
   }
 
   Widget button(String buttonText) {
     return new Expanded(
         child: ButtonTheme(
-      height: MediaQuery.of(context).size.height / 5 * 3 / 5,
-      child: FlatButton(
-        color: Colors.grey[900],
-        child: Text(
-          buttonText,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
+          height: MediaQuery
+              .of(context)
+              .size
+              .height / 5 * 3 / 5,
+          child: FlatButton(
+            color: Colors.grey[900],
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => buttonPressed(buttonText),
           ),
-        ),
-        onPressed: () => buttonPressed(buttonText),
-      ),
-    ));
+        ));
   }
 
   Widget specialButton(String buttonText) {
     return new Expanded(
         child: ButtonTheme(
-      height: MediaQuery.of(context).size.height / 5 * 3 / 5,
-      child: FlatButton(
-        color: Colors.grey[900],
-        child: Text(
-          buttonText,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 14.0,
-            fontWeight: FontWeight.bold,
+          height: MediaQuery
+              .of(context)
+              .size
+              .height / 5 * 3 / 5,
+          child: FlatButton(
+            color: Colors.grey[900],
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () => specialButtonPressed(buttonText),
           ),
-        ),
-        onPressed: () => buttonPressed(buttonText),
-      ),
-    ));
+        ));
   }
 
   Widget blank() {
     return new Expanded(
       child: Container(
-        height: MediaQuery.of(context).size.height / 5 * 3 / 5,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height / 5 * 3 / 5,
         color: Colors.grey[900],
         child: Text(
           '',
@@ -280,7 +356,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: [
                     specialButton('CLR'),
                     specialButton('DEL'),
-                    blank(),
+                    specialButton('90%'),
                     button("\u{00F7}"),
                   ],
                 ),
